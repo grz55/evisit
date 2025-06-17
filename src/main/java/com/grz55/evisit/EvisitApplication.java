6bf8bc1db7c9450dd901df38d2fecf9ac22f3207
grz55 <grz55@vp.pl>
2025-06-18 00:19:10 +0200

    Initial commit

---
+++ src/main/java/com/grz55/evisit/EvisitApplication.java
@@ -0,0 +1,13 @@
+package com.grz55.evisit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvisitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvisitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grz55/evisit/EvisitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grz55.evisit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvisitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
